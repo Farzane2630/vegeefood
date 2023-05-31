c7c09b372092c7e6b3c2c3d3e985a46bb9770bc8
Farzaneh <farzanekazemi8517@Gmail.com>
2023-05-31 17:10:02 +0330

    fix some tiny bugs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { useEffect, useState } from "react";
 import AllRoutes from "./Routes";
 import store from "./Redux/stores";
-import { Provider } from "react-redux";
-import { cartContext } from "./Contexts/Contexts";
+import { Provider } from "react-redux"; 
+import { useParams } from "react-router";
 
 export default function App() {
-  const [productQuantity, setProductQuantity] = useState(1);
+  const param = useParams();
+  useEffect(
+    () => {
+      // scroll to top on page load
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    },
+    [],
+    param
+  );
 
-  useEffect(() => {
-    // scroll to top on page load
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  }, []);
   return (
-    <cartContext.Provider
-      value={{
-        productQuantity,
-        setProductQuantity,
-      }}
-    >
-      <Provider store={store}>
-        <AllRoutes />
-      </Provider>
-    </cartContext.Provider>
+    <Provider store={store}>
+      <AllRoutes />
+    </Provider>
   );
 }
